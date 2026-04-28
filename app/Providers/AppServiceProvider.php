266bd8b0e5753f91c7ea92c8729914585328b347
John Lei Ulit <john.ulit@oqulo.com>
2026-04-28 23:27:16 +0800

    add https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\ServiceProvider;
 use App\View\Composers\UnviewedApplicationsComposer;
 use App\View\Composers\UnreadMessagesComposer;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\URL;
 
 
 class AppServiceProvider extends ServiceProvider
